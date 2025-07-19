37268ef71ddf0adede25ae66474c7bc04972273e
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-07-19 09:42:25 +0530

    Add files via upload

---
+++ nanddoubleexist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for(int i=0; i<arr.size(); i++){
+            if(s.find(2*arr[i])!=s.end())return true;
+            else if(arr[i]%2==0 && s.find(arr[i]/2)!=s.end())return true;
+            else s.insert(arr[i]);
+        }
+        return false;
+    }
+};
