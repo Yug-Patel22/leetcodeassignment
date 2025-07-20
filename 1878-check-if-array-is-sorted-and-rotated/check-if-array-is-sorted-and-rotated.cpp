41a7d15d53127a877245c4b08bd4605c668ab3d3
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-07-20 18:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (96.31%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int maxi=-1;
-        bool flow=true;
+        int c=0;
         for(int i=0; i<nums.size()-1; i++){
-            if(nums[i+1]<nums[i] && flow){
-                maxi=nums[i];
-                flow=false;
-            }
-            else if(nums[i+1]<nums[i] && !flow)return false;
-            else if(!flow && nums[i]>maxi)return false;
-            else if(!flow && nums[i]==maxi && nums[i+1]<maxi)return false;
+            if(nums[i]>nums[i+1])c++;
         }
-        if(!flow && nums[nums.size()-1]>maxi || !flow && nums[0]<nums[nums.size()-1])return false;
-        else if(!flow && nums[nums.size()-1]==maxi && nums[0]<maxi)return false;
-        return true;
+        if(nums[nums.size()-1]>nums[0])c++;
+        return c<=1;
     }
 };
