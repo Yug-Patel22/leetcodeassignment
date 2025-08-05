fd5ded04bb0c08900be36f1f79d48ca752a13787
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-08-05 21:43:03 +0530

    Time: 47 ms (57.36%) | Memory: 50.9 MB (23.16%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> um;
-        um[0] = 1;  // base case
+        um[0] = 1;
         int sum = 0, count = 0;
 
         for (int num : nums) {
             sum += num;
-            count += um[sum - k]; // all previous prefix sums that match
+            count += um[sum - k];
             um[sum]++;
         }
 
