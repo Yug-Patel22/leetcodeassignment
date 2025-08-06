5f49c349da0568f52fea39d86fe266daaa0cb584
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-08-06 12:03:01 +0530

    Time: 4 ms (56.40%) | Memory: 109.3 MB (89.21%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- //bfs queue
+ //bfs queue<TreeNode*>
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
