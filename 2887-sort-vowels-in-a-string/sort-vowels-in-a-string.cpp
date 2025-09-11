cbd50b254b2d1aff3ab7af85d9bb16993badcc46
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-09-11 22:15:14 +0530

    Time: 25 ms (15.63%) | Memory: 19.1 MB (14.47%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isVowel(char a){
+        a=toupper(a);
+        if(a=='A' || a=='E' || a=='I' || a=='O' || a=='U')return true;
+        return false;
+    }
+    string sortVowels(string s) {
+        vector<int>v;
+        for(int i=0; i<s.length(); i++){
+            if(isVowel(s[i])){
+                int a=s[i];
+                v.push_back(a);
+            }
+        }
+        sort(v.begin(),v.end());
+        int k=0;
+        for(int i=0; i<s.length(); i++){
+            if(isVowel(s[i])){
+                char a=v[k++];
+                s[i]=a;
+            }
+        }
+        return s;
+    }
+};
