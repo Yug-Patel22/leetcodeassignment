f333816abc82508d99e6f5a4bb1844952ddf362f
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-10-17 17:26:58 +0530

    Time: 3 ms (50.33%) | Memory: 24 MB (59.03%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -2,15 +2,22 @@ class NumArray {
     vector<int>v;
 public:
     NumArray(vector<int>& nums) {
-        v=nums;
+        v.resize(nums.size());
+        v[0]=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            v[i]=v[i-1]+nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left; i<=right; i++){
-            sum+=v[i];
-        }
-        return sum;
+        // int sum=0;
+        // for(int i=left; i<=right; i++){
+        //     sum+=v[i];
+        // }
+        // return sum;
+        if(left==0)return v[right];
+        else return v[right]-v[left-1];
+        return 0;
     }
 };
 
