1059cd9ae1b8cdbb7a688447b19c9ff787442b89
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-11-15 22:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (34.78%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int target=nums.size()-1;
-        for(int i=nums.size()-2; i>=0; i--){
-            if(i+nums[i]>=target)target=i;
+        // int target=nums.size()-1;
+        // for(int i=nums.size()-2; i>=0; i--){
+        //     if(i+nums[i]>=target)target=i;
+        // }
+        // if(target==0)return true;
+        // return false;
+        int maxi=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>maxi)return false;
+            maxi=max(maxi,i+nums[i]);
         }
-        if(target==0)return true;
-        return false;
+        return true;
     }
 };
