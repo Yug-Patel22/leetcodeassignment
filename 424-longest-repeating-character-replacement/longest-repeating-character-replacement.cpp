f405ce384400e2ed490bd075dbe43f296d770ad2
Yug-Patel22 <yugnpatel2236@gmail.com>
2025-12-24 15:09:36 +0530

    Time: 13 ms (21.40%) | Memory: 10.9 MB (69.19%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,16 +3,20 @@ public:
     int characterReplacement(string s, int k) {
         vector<int>v(26,0);
         int c=0,maxlen=0;
+        int maxi=v[0];
+        char main='A';
         for(int i=0; i<s.length(); i++){
             v[s[i]-'A']++;
-            int maxi=v[0];
-            char main='A';
-            for(int x=1; x<v.size(); x++){
-                if(v[x]>maxi){
-                    maxi=v[x];
-                    main=x+'A';
-                }
+            if(v[s[i]-'A']>maxi){
+                maxi=v[s[i]-'A'];
+                main=s[i];
             }
+            // for(int x=1; x<v.size(); x++){
+            //     if(v[x]>maxi){
+            //         maxi=v[x];
+            //         main=x+'A';
+            //     }
+            // }
             long long sum=accumulate(v.begin(),v.end(),0);
             int others=sum-v[main-'A'];
             if(others<=k){
