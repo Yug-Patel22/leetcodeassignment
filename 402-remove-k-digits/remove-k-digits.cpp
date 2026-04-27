e3646fd89a92c0b01dc88146a9715023695a32b1
Yug-Patel22 <yugnpatel2236@gmail.com>
2026-04-27 16:59:05 +0530

    Time: 9 ms (9.39%) | Memory: 11.5 MB (46.43%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    string remove_zero(string a){
-        int c = 0, n = a.length();
-        while (c < n && a[c] == '0') c++;
-        if (c == n) return "0";
-        return a.substr(c);
+    string remove_zero(string s){
+        int c=0;
+        while(c<s.length() && s[c]=='0')c++;
+        if(c==s.length())return "0";
+        return s.substr(c);
     }
     string removeKdigits(string num, int k) {
-        if(num.length()==k)return "0";
         int n=num.length();
-        if(num[0]=='0'){
-            num=remove_zero(num);
-        }
+        if(n==k || n==0)return "0";
+        if(num[0]=='0')num=remove_zero(num);
         n=num.length();
-        stack <char>s;
+        stack<char>s;
         for(int i=0; i<n; i++){
             while(!s.empty() && s.top()>num[i] && k>0){
                 s.pop();
@@ -21,7 +19,7 @@ public:
             }
             s.push(num[i]);
         }
-        while (k > 0 && !s.empty()) {
+        while(!s.empty() && k>0){
             s.pop();
             k--;
         }
