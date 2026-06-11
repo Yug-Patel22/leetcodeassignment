086c6bb25fc71b7d40c2ead6c1145ef872e5dc3b
Yug-Patel22 <yugnpatel2236@gmail.com>
2026-06-11 09:56:44 +0530

    Time: 3 ms (18.57%) | Memory: 8.8 MB (80.33%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int c=0;
+        int c=0; 
         string ans="";
         for(int i=0; i<s.length(); i++){
             if(s[i]=='(')c++;
