d17fb4bbbc1b3cb04258b23ecf54e5ac3a0f0655
Yug-Patel22 <yugnpatel2236@gmail.com>
2026-06-15 11:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (13.63%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,35 +1,65 @@
 class MyQueue {
-    stack <int> s1;
-    stack <int> s2;
+    
+    stack<int>s1,s2;
+
 public:
+
     MyQueue() {
         
     }
-    
+
     void push(int x) {
-        s1.push(x);
-    }
-    
-    int pop() {
-        int x=peek();
-        s2.pop();
-        return x;
-    }
-    
-    int peek() {
-        if(s2.empty()){
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
+        if(s1.empty() && s2.empty())s1.push(x);
+        else{
+            if(!s1.empty()){
+                while(!s1.empty()){
+                    s2.push(s1.top());
+                    s1.pop();
+                }
+                s1.push(x);
+                while(!s2.empty()){
+                    s1.push(s2.top());
+                    s2.pop();
+                }
             }
+            // else if(!s2.empty()){
+            //     while(!s2.empty()){
+            //         s1.push(s2.top());
+            //         s2.pop();
+            //     }
+            //     s2.push(x);
+            //     while(!s1.empty()){
+            //         s2.push(s1.top());
+            //         s1.pop();
+            //     }
+            // }
         }
-        return s2.top();
     }
-    
+
+    int pop() {
+        int a=NULL;
+        if(!s1.empty()){
+            a=s1.top();
+            s1.pop();
+        }
+        else if(!s2.empty()){
+            a=s2.top();
+            s2.pop();
+        }
+        return a;
+    }
+
+    int peek() {
+        if(!s1.empty())return s1.top();
+        else if(!s2.empty())return s2.top();
+        return NULL;
+    }
+
     bool empty() {
-        if(s2.empty() && s1.empty())return true;
+        if(s1.empty() && s2.empty())return true;
         return false;
     }
+
 };
 
 /**
